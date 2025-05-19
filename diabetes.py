5b59f56dece162e585423c4a077d1cb8f545b416
charles-balingit <charles.balingit28@gmail.com>
2025-05-19 22:41:48 +0800

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+import joblit
 
-def load_model():
-    return tf.keras.models.load_model('diabetes_model.h5')
-# Load the model
-model = load_model()  
+model = joblit('diabetes_model.h5)  
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Diabetes Classification", layout="centered")
